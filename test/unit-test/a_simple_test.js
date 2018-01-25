b5c1b353ad954905261ea0e2294adacc15dec07b
Jamie Czuy <jamie.czuy@turner.com>
2018-01-25 14:08:33 -0500

    refactored - to get linter to pass

--- test/unit-test/a_simple_test.js
+++ test/unit-test/a_simple_test.js
@@ -1,11 +1,15 @@
-const Chai = require('chai')
+const assert = require('chai').assert;
+
+const mocha = require('mocha');
+const describe = mocha.describe;
+const it = mocha.it;
 
 
 describe('A simple test - that always passes', () => {
-  it('is always true', (done) => {
-    Chai.assert.equal(true, true);
-    done();
-  });
+    it('is always true', (done) => {
+        assert.equal(true, true);
+        done();
+    });
 });
 
 
